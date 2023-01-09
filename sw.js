604718246920964a95d90dfa8cef057d096d1234
drowning-in-codes <bukalala174@gmail.com>
2023-01-09 09:59:23 +0800

    Site updated: 2023-01-09 09:58:53

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [];
+var precacheConfig = [["/sw-register.js","d86b9f99220b3a4afce7357ba2afb339"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
