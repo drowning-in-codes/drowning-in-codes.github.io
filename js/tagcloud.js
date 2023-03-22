bf3e17971d5c37e5e0fcbe7284b49b1b72f48326
drowning-in-codes <bukalala174@gmail.com>
2023-03-22 13:35:41 +0800

    Site updated: 2023-03-22 13:35:38

--- js/tagcloud.js
+++ js/tagcloud.js
@@ -14,11 +14,11 @@
      console.log('tag cloud plugin rock and roll!');
 
      try {
-         TagCanvas.textFont = 'Trebuchet MS, Helvetica';
+         TagCanvas.textFont = 'Helvetica';
          TagCanvas.textColour = '#333';
-         TagCanvas.textHeight = 25;
-         TagCanvas.outlineColour = '#E2E1D1';
-         TagCanvas.maxSpeed = 0.5;
+         TagCanvas.textHeight = 15;
+         TagCanvas.outlineColour = '#E2E1C1';
+         TagCanvas.maxSpeed = 0.03;
          TagCanvas.freezeActive = true;
          TagCanvas.outlineMethod = 'block';
          TagCanvas.minBrightness = 0.2;
@@ -39,6 +39,7 @@
          TagCanvas.tc['resCanvas'].Wheel(true)
      } catch(e) {
          console.log(e);
+		 console.log('tag cloud load failed')
          document.getElementById('myCanvasContainer').style.display = 'none';
      }
  });
