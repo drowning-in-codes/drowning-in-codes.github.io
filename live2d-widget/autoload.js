ff58da6153160e556df8e610957a6bc0a6c040a7
drowning-in-codes <bukalala174@gmail.com>
2023-08-02 13:28:35 +0800

    Site updated: 2023-08-02 13:27:40

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -34,8 +34,8 @@ if (screen.width >= 768) {
 		// 配置选项的具体用法见 README.md
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
-			//apiPath: "https://live2d.fghrsh.net/api/",
-			cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/",
+			apiPath: "https://live2d.fghrsh.net/api/",
+			//cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/",
 			tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
 		});
 	});
